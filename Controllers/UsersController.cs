b519f9145996253bfff4d0679b9e94c60203e54e
Stanly Alejandro <stanlie0314@gmail.com>
2022-08-04 13:13:12 -0500

    Registro

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetFullStack.Business.Abstract;
+using NetFullStack.DAL.Entities.DTO_S;
+using NetFullStack.DAL.Entities.Models;
+
+namespace NETFullStack.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly IUserAbstract _context;
+
+        public UsersController(IUserAbstract context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+
+
+        public async Task<ActionResult> Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Registrar(Usuarios userdto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Registrar(userdto);
+                await _context.GuardarCambios();
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NETFullStack.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
